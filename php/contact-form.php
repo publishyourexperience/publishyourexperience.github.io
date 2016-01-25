45b23430c8b717725892af80ae461b36ce62be66
Omar Abdalla <omar.abdalla@springer.com>
2016-01-25 13:56:11 +0000

    default template

---
+++ php/contact-form.php
@@ -0,0 +1,3 @@
+<?php
+    // Please buy premium version to get php files and documentation + more features
+    // visit: https://themewagon.com/themes/boots4-first-ever-free-bootstrap-4-template-in-the-world/
